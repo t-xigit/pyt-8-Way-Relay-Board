cd492e329516c52c72847490b3103d379501b9e3
Timur <timur@xigit.de>
2020-06-14 15:29:06 +0100

    pytest works again

--- k8_box.py
+++ k8_box.py
@@ -5,6 +5,7 @@ Module Docstring
 
 import serial
 import time
+import sys
 import logging
 import click
 
@@ -12,13 +13,22 @@ __author__ = "Timur Yigit"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-""" Data Frame """
-""" BYTE 0  - BYTE 1        - BYTE 2 - BYTE 3 """
-""" Command - Board address - Data   - Check sum XOR BYTE 1 BYTE 2 BYTE 3 """
 
 logging.basicConfig(level=logging.DEBUG)
 
-BOARD_ADDRESS = 1
+# Parameters for the serial inteface
+BAUDRATE = 19200
+TIMEOUT = 5
+BYTESIZE = 8
+STOPBITS = 1
+PARITY = 'N'
+
+serial_port = None
+
+""" Data Frame """
+""" BYTE 0  - BYTE 1        - BYTE 2 - BYTE 3 """
+""" Command - Board address - Data   - Check sum XOR BYTE 1 BYTE 2 BYTE 3 """
+# Protokol parameters for the relay box
 FRAME_SIZE = 4
 CMD = 0
 ADDR = 1
@@ -32,6 +42,9 @@ K5 = 16
 K6 = 32
 K7 = 64
 K8 = 128
+
+
+board_address = 0
 
 NOP = {
     "name": "NOP",
@@ -52,13 +65,13 @@ GET_PORT = {
 }
 
 SET_PORT = {
-    "name": "GET_PORT",
+    "name": "SET_PORT",
     "cmd": 3,
     "resp": 252
 }
 
 GET_OPTION = {
-    "name": "GET_PORT",
+    "name": "GET_OPTION",
     "cmd": 4,
     "resp": 251
 }
@@ -86,15 +99,6 @@ TOGGLE = {
     "cmd": 8,
     "resp": 247
 }
-
-logging.debug("Opening Serial Port")
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-ser.baudrate = 19200
-ser.timeout = 5
-ser.bytesize = 8
-ser.stopbits = 1
-ser.parity = 'N'
-print(ser)         # check which port was really used
 
 
 def get_crc(data):
@@ -127,11 +131,12 @@ def send_command(command, address, data):
     crc = get_crc(cmd)
     cmd.append(crc)
     start_time = time.time()
-    ser.write(cmd)     # write a string
+    serial_port.write(cmd)     # write a string
     end_time = time.time()
     logging.debug("Execution time send: %f", end_time - start_time)
     start_time = time.time()
-    response = ser.read(FRAME_SIZE)
+    response = serial_port.read(FRAME_SIZE)
+    logging.debug("Received Response: %s", response)
     end_time = time.time()
     logging.debug("Execution time read: %f", end_time - start_time)
     logging.debug("Received response: %s", response)
@@ -145,12 +150,14 @@ def get_data(command, address, data):
     crc = get_crc(cmd)
     cmd.append(crc)
     start_time = time.time()
-    ser.write(cmd)     # write a string
+    logging.debug("Sending Comand: %s", cmd)
+    serial_port.write(cmd)
     end_time = time.time()
     logging.debug("Execution time send: %f", end_time - start_time)
     start_time = time.time()
     # Before the Address is not set response is 8 bytes
-    response = ser.read(FRAME_SIZE * 2)
+    response = serial_port.read(FRAME_SIZE * 2)
+    logging.debug("Received Response: %s", response)
     check_response(command, response)
     end_time = time.time()
     logging.debug("Execution time read: %f", end_time - start_time)
@@ -171,35 +178,35 @@ def set_relay(relay, state):
         raise ValueError('Only relay 1 to 8 available')
     bit = 1 << relay
     if state == 1:
-        send_command(SET_SINGLE, BOARD_ADDRESS, bit)
+        send_command(SET_SINGLE, board_address, bit)
     if state == 0:
-        send_command(DEL_SINGLE, BOARD_ADDRESS, bit)
+        send_command(DEL_SINGLE, board_address, bit)
 
 
 @cli.command("set-all-off", help="Sets all relays to off")
 def set_all_off():
-    send_command(SET_PORT, BOARD_ADDRESS, 0)
+    send_command(SET_PORT, board_address, 0)
 
 
 @cli.command("test-all", help="Tests all relays by tuning them on and off")
 def test_all():
     logging.info("Testing all relays")
-    send_command(SET_PORT, BOARD_ADDRESS, 0)
+    send_command(SET_PORT, board_address, 0)
     time.sleep(0.5)
     for i in range(8):
-        send_command(SET_SINGLE, BOARD_ADDRESS, (1 << i))
+        send_command(SET_SINGLE, board_address, (1 << i))
         time.sleep(0.3)
-        send_command(DEL_SINGLE, BOARD_ADDRESS, (1 << i))
+        send_command(DEL_SINGLE, board_address, (1 << i))
 
 
 def main():
     """ Main entry point of the app """
-    fw_version = get_data(SETUP, BOARD_ADDRESS, BOARD_ADDRESS)
+    fw_version = get_data(SETUP, board_address, board_address)
     logging.info("FW_Version: %d", fw_version)
-    send_command(SET_OPTION, BOARD_ADDRESS, 2)
-    send_command(NOP, BOARD_ADDRESS, 0)
-    send_command(SET_SINGLE, BOARD_ADDRESS, 8)
-    status = get_data(GET_PORT, BOARD_ADDRESS, 0)
+    send_command(SET_OPTION, board_address, 2)
+    send_command(NOP, board_address, 0)
+    send_command(SET_SINGLE, board_address, 8)
+    status = get_data(GET_PORT, board_address, 0)
     print(status)
     set_relay(1, 1)
     time.sleep(5)
@@ -207,7 +214,50 @@ def main():
     ser.close()             # close port
 
 
+def set_brodcast_opt():
+
+    logging.debug("Setting Broadcast Option to: %d", brdcast_opt)
+    send_command(SET_OPTION, board_address, brdcast_opt)
+
+
+def close_port(port):
+    logging.debug("Closing serial port")
+    port.close()
+    
+
+def open_port(port):
+    logging.debug("Opening Serial Port")
+    port = serial.Serial('/dev/ttyUSB0')  # open serial port
+    port.baudrate = BAUDRATE
+    port.timeout = TIMEOUT
+    port.bytesize = BYTESIZE
+    port.stopbits = STOPBITS
+    port.parity = PARITY
+    logging.debug(port)
+    return port
+
+
+def init_relay_box():
+
+    global board_address
+
+    SET_BRD_ADDR = 1
+    logging.info("Initializing relay box with Address: %d", SET_BRD_ADDR)
+    # Setup command initalizes the board the address, and returns FW Version
+    fw_version = get_data(SETUP, SET_BRD_ADDR, SET_BRD_ADDR)
+    #logging.info("FW_Version: %d", fw_version)
+    brdcast_opt = get_data(GET_OPTION, SET_BRD_ADDR , SET_BRD_ADDR)
+    logging.info("Broadcast Option: %d", brdcast_opt)
+    brdcast_opt = 2
+    logging.debug("Setting Broadcast Option to: %d", brdcast_opt)
+    #send_command(SET_OPTION, SET_BRD_ADDR, brdcast_opt)
+    board_address = SET_BRD_ADDR
+
+
+serial_port = open_port(serial_port)
+init_relay_box()
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     cli()
-    # main()
+    serial_port.close()             # close port
+    sys.exit(0)
